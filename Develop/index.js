36c04383bceb9fbe53fbca9054e036110cd68788
MeshalS <meshal.saus@gmail.com>
2020-10-15 19:44:40 +0300

    adding the Question

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,25 @@
-// array of questions for user
+let  inquirer = require("inquirer");
+let fs = require("fs");
+let generateMarkdown = require("./generateMarkdown")
+
 const questions = [
+
+    {
+        type: "input",
+        message: "What is your user name in Github?",
+        name: "username"
+    }, {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    }, 
+{
+    type: "input",
+    message: "What is your project?",
+    name: "title"
+
+}
+    
 
 ];
 
